57c28b4bd2287cb2d3a8296e12939640c369fd9e
guyz <guy@shopic.co>
2023-10-26 18:20:32 +0300

    created <AppHeader> layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { AppHeader } from "./layout/AppHeader/AppHeader";
 import "./styles/reset.css";
+import './styles/base.css';
 
 export function App() {
   return (
--- src/layout/AppHeader/AppHeader.jsx
+++ src/layout/AppHeader/AppHeader.jsx
@@ -1,9 +1,37 @@
-import S from './AppHeader.module.scss';
+import S from "./AppHeader.module.scss";
+import { Typography } from "../../components/Typography/Typography";
+import { Button } from "../../components/Button/Button";
+import PlusSignSvg from "../../assets/img/plus-sign.svg?react";
+
+const appHeaderTextBase = "app_header";
 
 export function AppHeader() {
   return (
     <div className={S.header}>
-      header
+      <div className={S.instructions}>
+        <Typography
+          textPath={`${appHeaderTextBase}.app_title`}
+          size={24}
+          weight={500}
+        />
+        <Typography
+          textPath={`${appHeaderTextBase}.instructions_title`}
+          size={18}
+          weight={600}
+        />
+        <Typography
+          textPath={`${appHeaderTextBase}.instructions`}
+          className={S.info}
+        />
+      </div>
+      <Button>
+        <PlusSignSvg />
+        <Typography
+          textPath={`${appHeaderTextBase}.button`}
+          size={12}
+          weight={600}
+        />
+      </Button>
     </div>
   );
 }
