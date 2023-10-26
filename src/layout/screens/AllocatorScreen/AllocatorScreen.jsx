a053a0cae6b131dda52297a33d539931eb902d3c
guyz <guy@shopic.co>
2023-10-26 19:46:27 +0300

    created mock screens for navigation test

---
+++ src/layout/screens/AllocatorScreen/AllocatorScreen.jsx
@@ -0,0 +1,5 @@
+import S from "./AllocatorScreen.module.scss";
+
+export function AllocatorScreen() {
+  return <div className={S.allocatorScreen}>allocator</div>;
+}
---
+++ src/layout/screens/SummaryScreen/SummaryScreen.jsx
@@ -0,0 +1,5 @@
+import S from "./SummaryScreen.module.scss";
+
+export function SummaryScreen() {
+  return <div className={S.summaryScreen}>summmmry</div>;
+}
