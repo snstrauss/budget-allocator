eb4ae0c218ab80c94cfef14b3f11471efa8e54ec
guyz <guy@shopic.co>
2023-10-27 13:34:39 +0300

    <AllocatorScreen> now shows a list of <BudgetChannel>s

--- src/layout/screens/AllocatorScreen/AllocatorScreen.jsx
+++ src/layout/screens/AllocatorScreen/AllocatorScreen.jsx
@@ -1,5 +1,23 @@
+import { useContext, useState } from "react";
+import { BudgetChannelsContext } from "../../../contexts/budgetChannelsContext";
 import S from "./AllocatorScreen.module.scss";
+import { BudgetChannel } from "../../../components/BudgetChannel/BudgetChannel";
 
 export function AllocatorScreen() {
-  return <div className={S.allocatorScreen}>allocator</div>;
+  const { state: budgetChannels } = useContext(BudgetChannelsContext);
+
+  const [openIndex, setOpenIndex] = useState(0);
+
+
+  return (
+    <div className={S.allocatorScreen}>
+      {budgetChannels.map((channelData, idx) => (
+        <BudgetChannel
+          channelData={channelData}
+          isOpen={idx === openIndex}
+          onSelectChannel={() => setOpenIndex(idx)}
+        />
+      ))}
+    </div>
+  );
 }
