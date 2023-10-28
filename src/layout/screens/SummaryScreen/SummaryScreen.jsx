c1780c2d623b8107bc6ae55b33289b4b467b6094
guyz <guy@shopic.co>
2023-10-28 21:25:14 +0300

    very rudimentary summary screen - it has access to the channels

--- src/layout/screens/SummaryScreen/SummaryScreen.jsx
+++ src/layout/screens/SummaryScreen/SummaryScreen.jsx
@@ -1,5 +1,33 @@
+import { useContext } from "react";
 import S from "./SummaryScreen.module.scss";
+import {
+  BudgetChannelsContext,
+  useBudgetChannel,
+} from "../../../contexts/budgetChannelsContext";
 
 export function SummaryScreen() {
-  return <div className={S.summaryScreen}>summmmry</div>;
+  const { state: channels } = useContext(BudgetChannelsContext);
+
+  return (
+    <div className={S.summaryScreen}>
+      {channels.map(({ id }) => (
+        <ChannelSummary key={id} channelId={id} />
+      ))}
+    </div>
+  );
+}
+
+function ChannelSummary({ channelId }) {
+  const { name, months } = useBudgetChannel(channelId);
+
+  return (
+    <div style={{ border: "2px solid red" }}>
+      <div>{name}</div>
+      {
+        months.map(val => (<span style={{margin: 5}}>
+          {val}
+        </span>))
+      }
+    </div>
+  );
 }
